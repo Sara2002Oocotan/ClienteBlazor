53a2e250a5ec83dc8fe61d795fddc95ede9f0623
Sara <om21002ts@esfe.agape.edu.sv>
2022-10-03 15:15:11 +0100

    Se agrega el proyecto cliente blazor

---
+++ ClienteBlazor.AccesoriosMoviles/Services/ProductoService.cs
@@ -0,0 +1,43 @@
+﻿using ClienteBlazor.AccesoriosMoviles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClienteBlazor.AccesoriosMoviles.Services
+{
+    public class ProductoService : IProductoService
+    {
+        //inyección de dependencias de HttpClient
+        private readonly HttpClient _httpClient;
+        public ProductoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        //configurar las opciones del Serializador
+        JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+        public async Task<IEnumerable<Producto>> GetAll()
+        {
+            string resp = await _httpClient.GetStringAsync($"Producto");
+            return JsonSerializer.Deserialize<IEnumerable<Producto>>(resp, options);
+        }
+
+        public async Task<IEnumerable<Producto>> GetByCategoria(int idCategoria)
+        {
+            var resp = await _httpClient.PostAsJsonAsync($"Producto/Buscar", new { idCategoria = idCategoria });
+            string respString = await resp.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<IEnumerable<Producto>>(respString, options);
+        }
+
+        public async Task<Producto> GetById(int id)
+        {
+            string resp = await _httpClient.GetStringAsync($"Producto/{id}");
+            return JsonSerializer.Deserialize<Producto>(resp, options);
+        }
+    }
+}
